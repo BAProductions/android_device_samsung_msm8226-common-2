e8f16c24111d2d17c96611155ea87d3a967d61e7
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-12-16 15:43:35 -0500

    VENDOR_PATH > LOCAL_PATH
    VENDOR_PATH > LOCAL_PATH

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,22 +15,22 @@
 # inherit from qcom-common
 -include device/samsung/qcom-common/BoardConfigCommon.mk
 
-VENDOR_PATH := device/samsung/msm8226-common
+LOCAL_PATH := device/samsung/msm8226-common
 
 include device/samsung/msm8226-common/board/*.mk
 
-TARGET_SPECIFIC_HEADER_PATH := $(VENDOR_PATH)/include
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 #TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 #TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
 #TODO: Check
 # CMHW
-#BOARD_HARDWARE_CLASS += $(VENDOR_PATH)/cmhw
+#BOARD_HARDWARE_CLASS += $(LOCAL_PATH)/cmhw
 
 #TODO: Check
 # Custom RIL class
-#BOARD_RIL_CLASS := ../../../$(VENDOR_PATH)/ril
+#BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
 
 # QCOM Hardware
 BOARD_USES_QCOM_HARDWARE := true
@@ -38,9 +38,10 @@ BOARD_USES_QCOM_HARDWARE := true
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
 
+#TODO: Check
 # Properties (reset them here, include more in device if needed)
-#TARGET_SYSTEM_PROP := $(VENDOR_PATH)/system.prop
+#TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
