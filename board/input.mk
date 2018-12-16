fb762923e45b0e8bf520b6c9b0ff075742591c24
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-12-16 13:05:06 -0500

    Adding idc
    Adding idc

---
+++ board/input.mk
@@ -0,0 +1,6 @@
+# Input device
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/idc/sec_e-pen.idc:system/usr/idc/sec_e-pen.idc \
+	$(LOCAL_PATH)/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc \
+	$(LOCAL_PATH)/idc/Synaptics_HID_TouchPad.idc:system/usr/idc/Synaptics_HID_TouchPad.idc \
+	$(LOCAL_PATH)/idc/Synaptics_RMI4_TouchPad_Sensor.idc:system/usr/idc/Synaptics_RMI4_TouchPad_Sensor.idc
