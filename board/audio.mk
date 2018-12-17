37de5b400ff282d7bcb7b9a320ab6eb673f896f9
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-12-16 22:43:09 -0500

    Adding Media, Audio & GPS config file to board repo
    Adding Media, Audio & GPS config file to board repo

--- board/audio.mk
+++ board/audio.mk
@@ -1,2 +1,8 @@
 # Audio
+AUDIO_FEATURE_ENABLED_LOW_LATENCY_CAPTURE := true
 BOARD_USES_ALSA_AUDIO := true
+
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
---
+++ board/gps.mk
@@ -0,0 +1,3 @@
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/flp.conf:system/etc/flp.conf
